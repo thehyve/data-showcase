00970cb7cb7894bc630cb99a2e2e6097e42c6194
ewelinagr <ewelina@thehyve.nl>
2017-09-07 08:58:50 +0200

    Add export functionality

--- src/main/user-interface/src/app/shopping-cart/shopping-cart.component.ts
+++ src/main/user-interface/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from "../services/data.service";
 import {Item} from "../models/item";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,11 +12,15 @@ export class ShoppingCartComponent implements OnInit {
 
   display: boolean = false;
   items: Item[] = [];
+  pathSelection: string[] = [];
 
   constructor(private dataService: DataService) {
     dataService.shoppingCartItems$.subscribe(
       items => {
         this.items = items;
+        this.pathSelection = items.map(function(item) {
+          return item['domain'];
+        });
       });
   }
 
@@ -29,5 +34,11 @@ export class ShoppingCartComponent implements OnInit {
   deleteItem(itemToDelete: Item) {
     this.items = this.items.filter(item => item !== itemToDelete);
     this.dataService.setShoppingCartItems(this.items);
+  }
+
+  exportItems(){
+    let exportObject = {paths: this.pathSelection};
+    let file = new Blob([JSON.stringify(exportObject)], { type: 'text/json;charset=utf-8' });
+    saveAs(file, 'dsc_selection.json');
   }
 }
