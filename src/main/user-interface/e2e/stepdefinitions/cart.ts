ee26e05ffe0c59d1d446422bcbb23c2471d2151b
barteld klasens <barteld@thehyve.nl>
2017-10-17 16:47:03 +0200

    add, remove, export, cart

---
+++ src/main/user-interface/e2e/stepdefinitions/cart.ts
@@ -0,0 +1,48 @@
+import { $, browser } from 'protractor';
+import { checkTextElement, countIs } from './support/util';
+
+var fs = require('fs');
+let { defineSupportCode } = require('cucumber');
+
+defineSupportCode(({ Given, When, Then }) => {
+
+  When('I add them to the cart', function () {
+    return $('[label="Add to cart"]').click();
+  });
+
+  When('I open the cart', function () {
+    return $('.ds-shopping-cart').click();
+  });
+
+  When('I remove all from the cart', function () {
+    return $('[label="Delete all"]').click();
+  });
+
+  Then('the cart contains', function (string) {
+    let tableRows: [string] = JSON.parse(string);
+    return countIs($('.ds-shopping-cart').$$(".ui-datatable-data > tr"), tableRows.length).then(() => {
+      return $('.ds-shopping-cart').$$(".ui-datatable-data > tr").map((row, rowIndex) => { // get all data rows
+        return row.$$('.ui-cell-data').map((cell, cellIndex) => {
+          return checkTextElement(cell, tableRows[rowIndex][cellIndex]);
+        })
+      })
+    })
+  });
+
+  Then('the cart is empty', function () {
+    return countIs($('.ds-shopping-cart').$$(".ui-datatable-data > tr"), 0);
+  });
+
+  When('I export', function () {
+    return $('[placeholder="File name (optional)"]').sendKeys('testDownload').then(() => {
+      return $('[label="Export"]').click();
+    })
+  });
+
+  Then('a file is downloaded', function () {
+    return browser.driver.wait(function () {
+      return fs.existsSync(__dirname + '/../downloads/' + 'testDownload.json');
+    }, 30000)
+  });
+
+});
--- src/main/user-interface/e2e/stepdefinitions/support/hooks.ts
+++ src/main/user-interface/e2e/stepdefinitions/support/hooks.ts
@@ -1,10 +1,14 @@
 import { Promise } from 'es6-promise';
 import { browser } from 'protractor';
+import * as fs from 'fs';
 
 let { defineSupportCode } = require('cucumber');
 
 defineSupportCode(function ({ After, Before }) {
   Before({}, function (scenario): Promise<any> {
+    if (fs.existsSync(__dirname + '/../../downloads/' + 'testDownload.json')){
+      fs.unlinkSync(__dirname + '/../../downloads/' + 'testDownload.json');
+    }
     return browser.get('')
   });
-})
+});
