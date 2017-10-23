e6cabda14fbdf2ec5970babd90d6d24ab1d6d1f1
Ewelina Grudzien <ewelina@thehyve.nl>
2017-10-23 09:23:49 +0200

    Move test data loading to a separate function in DataImportServiceSpec

--- src/test/groovy/nl/thehyve/datashowcase/DataImportServiceSpec.groovy
+++ src/test/groovy/nl/thehyve/datashowcase/DataImportServiceSpec.groovy
@@ -29,17 +29,32 @@ class DataImportServiceSpec extends Specification {
     def setupData() {
         log.info "Clear database ..."
         dataService.clearDatabase()
+    }
+
+    def loadValidData() {
         log.info "Upload test data set ..."
         JSONObject json = (JSONObject)JSON.parse(file.text)
         dataImportService.upload(json)
     }
 
+    def loadInvalidData() {
+        log.info "Replace with invalid data ..."
+        file.text = file.text.replaceAll('"publicItem": true', '"publicItem": false')
+        JSONObject json = (JSONObject)JSON.parse(file.text)
+        log.info "Upload test data set ..."
+        dataImportService.upload(json)
+    }
+
     @Requires({ -> Environment.grailsEnvironmentIn(Constants.PUBLIC_ENVIRONMENTS) })
     void "test json data upload"() {
-        given:
+        given: 'the database is clean'
         setupData()
-        when:
-        log.info "Running test ..."
+
+        when: 'loading valid data'
+        loadValidData()
+
+        // fetch objects from db
+        then: 'the data is stored in the database'
         def items = Item.getAll()
         def concepts = Concept.getAll()
         def keywords = Keyword.getAll()
@@ -49,7 +64,87 @@ class DataImportServiceSpec extends Specification {
         def treeNodes = TreeNode.getAll()
         def values = Value.getAll()
 
-        then: "All elements are uploaded correctly"
+        items.size() == 2
+        items*.name == ['ageA', 'heightB']
+        items*.itemPath == ['/Personal information/Basic information/Age',
+                            '/Personal information/Extended information/Height']
+        items.every { it.publicItem == true }
+        items*.summary == summaries
+        items*.summary*.values?.flatten() == values
+        items*.concept.every { concepts.indexOf(it) != -1 }
+        items*.project.every { projects.indexOf(it) != -1 }
+
+        concepts.size() == 3
+        concepts*.conceptCode == ['age', 'height', 'weight']
+        concepts*.label == ['Age', 'Height', 'Weight']
+        concepts*.labelLong == ['Age at time of survey', 'Height at time of survey', 'Weight (kg)']
+        concepts*.labelNl == ['Leeftijd', 'Hoogte', 'Gewicht']
+        concepts*.labelNlLong == ['Leeftijd van het onderwerp', 'Hoogte van het onderwerp', 'Gewicht (kg)']
+        concepts*.variableType == [VariableType.Categorical, VariableType.Categorical, VariableType.Numerical]
+        concepts*.keywords.flatten() as Set == keywords as Set
+
+        keywords.size() == 3
+        keywords*.keyword == ['Personal information', 'Family related', 'Body characteristics']
+
+        projects.size() == 2
+        projects*.name == ['Project A', 'Project B']
+        projects*.description == ['First test project', 'Second test project']
+        projects*.lineOfResearch == linesOfResearch
+
+        linesOfResearch.size() == 2
+        linesOfResearch*.name == ['Research line 1', 'Research line 2']
+
+        summaries.size() == 2
+        summaries*.patientCount == [100, 200]
+        summaries*.observationCount == [102, 402]
+        summaries*.dataStability == ['Committed', 'Committed']
+        summaries*.values.flatten() == values
+
+        treeNodes.size() == 9
+        treeNodes*.label.sort() == ['Personal information', 'Basic information', 'Age', 'Weight', 'Extended information',
+                                    'Some details', 'Height', 'Other information', 'Some information'].sort()
+        treeNodes.every { node ->
+            if (node.label in ['Age', 'Weight', 'Height']) {
+                node.nodeType == NodeType.Concept && node.concept != null && node.children.size() == 0 && node.parent != null
+            } else
+                node.nodeType == NodeType.Domain
+        }
+        treeNodes*.path.sort() == ['/Personal information',
+                                   '/Personal information/Basic information',
+                                   '/Personal information/Basic information/Age',
+                                   '/Personal information/Basic information/Weight',
+                                   '/Personal information/Extended information',
+                                   '/Personal information/Extended information/Height',
+                                   '/Personal information/Extended information/Some details',
+                                   '/Other information',
+                                   '/Other information/Some information'].sort()
+
+        values.size() == 8
+        values*.value.every { v -> v in ['<= 65', '> 65', '<= 175', '> 175'] }
+        values*.label.every { l -> l in ['Young', 'Old', 'Short', 'Tall'] }
+        values*.frequency.every { f -> (int)f in [35, 65, 36, 64] }
+    }
+
+    @Requires({ -> Environment.grailsEnvironmentIn(Constants.PUBLIC_ENVIRONMENTS) })
+    void "test json data reupload"() {
+        given: 'the database contains data'
+        setupData()
+        loadValidData()
+
+        when: 'reloading valid data'
+        loadValidData()
+
+        // fetch objects from db
+        then: 'the data is stored in the database'
+        def items = Item.getAll()
+        def concepts = Concept.getAll()
+        def keywords = Keyword.getAll()
+        def linesOfResearch = LineOfResearch.getAll()
+        def projects = Project.getAll()
+        def summaries = Summary.getAll()
+        def treeNodes = TreeNode.getAll()
+        def values = Value.getAll()
+
         items.size() == 2
         items*.name == ['ageA', 'heightB']
         items*.itemPath == ['/Personal information/Basic information/Age',
@@ -113,10 +208,12 @@ class DataImportServiceSpec extends Specification {
 
     @Requires({ -> Environment.grailsEnvironmentIn(Constants.PUBLIC_ENVIRONMENTS) })
     void "test publicItem flag validation"() {
-        given:
-        file.text = file.text.replaceAll('"publicItem": true', '"publicItem": false')
-        when:
+        given: 'the database is clean'
         setupData()
+
+        when: 'loading invalid data'
+        loadInvalidData()
+
         then:
         InvalidDataException ex = thrown()
         ex.message == "An error occured when uploading the data: Data validation exception. " +
