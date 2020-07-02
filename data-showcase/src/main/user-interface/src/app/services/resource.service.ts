bbf56f6191efe3dabbe2f6d3df61d309e5df7981
Gijs Kant <gijs@thehyve.nl>
2020-07-02 15:59:28 +0200

    Update user interface libraries.

--- data-showcase/src/main/user-interface/src/app/services/resource.service.ts
+++ data-showcase/src/main/user-interface/src/app/services/resource.service.ts
@@ -8,40 +8,45 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import {TreeNode} from "../models/tree-node";
-import {Http, Response, Headers, ResponseContentType, RequestOptions} from '@angular/http';
-import {Endpoint} from "../models/endpoint";
-import {AppConfig} from "../config/app.config";
+import {TreeNode} from '../models/tree-node';
+import {Endpoint} from '../models/endpoint';
+import {AppConfig} from '../config/app.config';
 import {
   PATH_CONCEPTS,
   PATH_ENVIRONMENT, PATH_ITEMS, PATH_LOGOS, PATH_PROJECTS,
   PATH_TREE_NODES,  PATH_KEYWORDS_BY_CONCEPT
-} from "../constants/endpoints.constants";
-import {Item} from "../models/item";
-import {Project} from "../models/project";
-import {Environment} from "../models/environment";
+} from '../constants/endpoints.constants';
+import {Item} from '../models/item';
+import {Project} from '../models/project';
+import {Environment} from '../models/environment';
 import { Concept } from '../models/concept';
-import {ItemResponse} from "../models/itemResponse";
+import {ItemResponse} from '../models/itemResponse';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 
 
 @Injectable()
 export class ResourceService {
 
-  private endpoint: Endpoint;
-
-  constructor(private http: Http, private appConfig: AppConfig) {
-    let apiUrl = appConfig.getConfig('api-url');
-    let apiVersion = appConfig.getConfig('api-version');
-    let appUrl = appConfig.getConfig('app-url');
+  constructor(private http: HttpClient, private appConfig: AppConfig) {
+    const apiUrl = appConfig.getConfig('api-url');
+    const apiVersion = appConfig.getConfig('api-version');
+    const appUrl = appConfig.getConfig('app-url');
     this.endpoint = new Endpoint(apiUrl, apiVersion, appUrl);
   }
 
-  private handleError(error: Response | any) {
+  private endpoint: Endpoint;
+
+  static getMockData(data: Object): Observable<object> {
+    // temporal test values
+    return Observable.of(data);
+  }
+
+  private static handleError<T>(error: HttpResponse<any> | any): Observable<T> {
     let errMsg: string;
-    if (error instanceof Response) {
-      let contentType = error.headers.get('Content-Type') || '';
-      if (contentType == 'application/json') {
-          const body = error.json() || '';
+    if (error instanceof HttpResponse) {
+      const contentType = error.headers.get('Content-Type') || '';
+      if (contentType === 'application/json') {
+          const body = error.body.json() || '';
           errMsg = body.error || JSON.stringify(body);
       } else {
         errMsg = `Error: ${error.statusText}`;
@@ -54,42 +59,47 @@ export class ResourceService {
       errMsg = error.message ? error.message : error.toString();
     }
     console.error(errMsg);
-    return Observable.throw(errMsg || 'Server error');
+    return Observable.throwError(errMsg || 'Server error');
   }
 
   getTreeNodes(): Observable<TreeNode[]> {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_TREE_NODES;
+    const headers = new HttpHeaders();
+    const url = this.endpoint.apiUrl + PATH_TREE_NODES;
 
     return this.http.get(url, {
-      headers: headers
+      headers: headers,
+      responseType: 'json'
     })
-      .map((response: Response) => response.json().tree_nodes as TreeNode[])
-      .catch(this.handleError.bind(this));
+    .map((response: any) => response.tree_nodes as TreeNode[])
+    .catch(err => ResourceService.handleError<TreeNode[]>(err));
   }
 
   getItem(id: number): Observable<Item> {
-    let headers = new Headers();
-    let url = `${this.endpoint.apiUrl}${PATH_ITEMS}/${id}`;
+    const headers = new HttpHeaders();
+    const url = `${this.endpoint.apiUrl}${PATH_ITEMS}/${id}`;
 
     return this.http.get(url, {
-      headers: headers
+      headers: headers,
+      responseType: 'json'
     })
-    .map((response: Response) => response.json() as Item)
-    .catch(this.handleError.bind(this));
+    .map((response: any) => response as Item)
+    .catch(err => ResourceService.handleError<Item>(err));
   }
 
 
-  getItems(firstResult: number, maxResults: number, order?: string,propertyName?: string,
+  getItems(firstResult: number, maxResults: number, order?: string, propertyName?: string,
            conceptCodes?: string[], projects?: string[], jsonSearchQuery?: Object): Observable<ItemResponse> {
 
-    let headers = new Headers();
+    const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
 
-    const options = new RequestOptions({headers: headers});
+    const options = {
+      headers: headers,
+      responseType: 'json' as 'json'
+    };
     const url = this.endpoint.apiUrl + PATH_ITEMS;
 
-    let body = {
+    const body = {
         conceptCodes: conceptCodes,
         projects: projects,
         searchQuery: jsonSearchQuery,
@@ -101,89 +111,86 @@ export class ResourceService {
 
     // use POST because of url length limits in some of the browsers (limit of characters)
     return this.http.post(url, body, options)
-      .map((res: Response) => res.json() as ItemResponse)
-      .catch(this.handleError.bind(this));
+      .map(res => res as ItemResponse)
+      .catch(err => ResourceService.handleError<ItemResponse>(err));
   }
 
   getAllProjects(): Observable<Project[]> {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_PROJECTS;
+    const url = this.endpoint.apiUrl + PATH_PROJECTS;
 
-    return this.http.get(url, {
-      headers: headers
-    })
-      .map((response: Response) => response.json().projects as Project[])
-      .catch(this.handleError.bind(this));
+    return this.http.get(url)
+      .map((response: HttpResponse<any>) => response.body.json().projects as Project[])
+      .catch(err => ResourceService.handleError<Project[]>(err));
   }
 
   getProjects(conceptCodes?: string[], jsonSearchQuery?: Object): Observable<Project[]> {
-    let headers = new Headers();
+    const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
 
-    const options = new RequestOptions({headers: headers});
+    const options = {
+      headers: headers,
+      responseType: 'json' as 'json'
+    };
     const url = this.endpoint.apiUrl + PATH_PROJECTS;
     let body = null;
 
-    if(conceptCodes || jsonSearchQuery) {
+    if (conceptCodes || jsonSearchQuery) {
       body = {
         conceptCodes: conceptCodes,
         searchQuery: jsonSearchQuery
-      }
+      };
     }
 
     // use POST because of url length limits in some of the browsers (limit of characters)
     return this.http.post(url, body, options)
-      .map((res: Response) => res.json().projects as Project[])
-      .catch(this.handleError.bind(this));
+      .map((res: any) => res.projects as Project[])
+      .catch(err => ResourceService.handleError<Project[]>(err));
   }
 
   getKeywords(conceptCode: string): Observable<string[]>  {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_KEYWORDS_BY_CONCEPT + "/" + conceptCode;
+    const headers = new HttpHeaders();
+    const url = this.endpoint.apiUrl + PATH_KEYWORDS_BY_CONCEPT + '/' + conceptCode;
 
     return this.http.get(url, {
-      headers: headers
-    })
-      .map((response: Response) => response.json().keywords as string[])
-      .catch(this.handleError.bind(this));
+        headers: headers,
+        responseType: 'json'
+      })
+      .map((response: any) => response.keywords as string[])
+      .catch(err => ResourceService.handleError<string[]>(err));
   }
 
   getConcepts(): Observable<Concept[]> {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_CONCEPTS;
+    const headers = new HttpHeaders();
+    const url = this.endpoint.apiUrl + PATH_CONCEPTS;
 
     return this.http.get(url, {
-      headers: headers
-    })
-      .map((response: Response) => response.json().concepts as Concept[])
-      .catch(this.handleError.bind(this));
+        headers: headers,
+        responseType: 'json'
+      })
+      .map((response: any) => response.concepts as Concept[])
+      .catch(err => ResourceService.handleError<Concept[]>(err));
   }
 
   getLogo(type: string): Observable<Blob> {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_LOGOS +"/" + type;
+    const headers = new HttpHeaders();
+    const url = this.endpoint.apiUrl + PATH_LOGOS + '/' + type;
 
     return this.http.get(url, {
-      headers: headers,
-      responseType: ResponseContentType.Blob
-    })
-      .map((response: Response) => response.blob())
-      .catch(this.handleError.bind(this));
+        headers: headers,
+        responseType: 'blob'
+      })
+      .catch(err => ResourceService.handleError<Blob>(err));
   }
 
   getEnvironment(): Observable<Environment> {
-    let headers = new Headers();
-    let url = this.endpoint.apiUrl + PATH_ENVIRONMENT;
+    const headers = new HttpHeaders();
+    const url = this.endpoint.apiUrl + PATH_ENVIRONMENT;
 
     return this.http.get(url, {
-      headers: headers
+      headers: headers,
+      responseType: 'json'
     })
-      .map((response: Response) => response.json() as Environment)
-      .catch(this.handleError.bind(this));
-  }
-
-  static getMockData(data: Object): Observable<object> {
-    //temporal test values
-    return Observable.of(data);
+    .map((response: any) => response as Environment)
+    .catch(err => ResourceService.handleError<Environment>(err));
   }
 }
